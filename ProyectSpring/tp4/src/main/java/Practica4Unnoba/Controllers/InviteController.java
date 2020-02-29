49b1d65ef0b9e87289b268409170001cec16cd6c
MDimase <matias_dimase10@hotmail.com>
2020-02-29 16:54:43 -0300

    view invitations.hmtl en desarrollo y rutas /addinvite/{eventId} a medias

--- ProyectSpring/tp4/src/main/java/Practica4Unnoba/Controllers/InviteController.java
+++ ProyectSpring/tp4/src/main/java/Practica4Unnoba/Controllers/InviteController.java
@@ -3,11 +3,10 @@ package Practica4Unnoba.Controllers;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
@@ -17,7 +16,8 @@ import Practica4Unnoba.Entities.Usuario;
 import Practica4Unnoba.Services.EventService;
 import Practica4Unnoba.Services.InviteService;
 import Practica4Unnoba.Services.UserService;
-@RestController
+
+@Controller
 public class InviteController {
 	
 	@Autowired
@@ -34,26 +34,25 @@ public class InviteController {
 		inviteService.deleteInvite(id);
 	}	
 	
-	@GetMapping("/addinvite")
-	public String showAddInviteForm(Model model) {
-		//guardo todos los eventos y usuarios en arreglos
-		List<Event> events = new ArrayList<Event>();
+	@GetMapping("/addinvite/{eventId}")
+	public String showAddInviteForm(Model model,@PathVariable Long eventId) {
+		//guardo todos los usuarios menos el owner de este evento en arreglo
 		List<Usuario> users = new ArrayList<Usuario>();
-		events.addAll(eventService.findAllEvents());
 		users.addAll(userService.getAllUsers());
-		
+		users.remove(userService.getUserLogged());
+		Event event = eventService.getEvent(eventId);
 		model.addAttribute("users", users);
-		model.addAttribute("events", events);
-		
-		System.out.println(users);
-		System.out.println(events);
+		model.addAttribute("event", event);
 		
 		return "invitations";
 	}
 	
 	@PostMapping("/addinvite")
-	public void addInvite (@RequestBody Invite invite) {
-		inviteService.addInvite(invite);
+	public String addInvite (Model model) {
+		System.out.println("post controller");
+		System.out.println(model.asMap());
+		//inviteService.addInvite(invite);
+		return "home";
 	}
 	
 	
